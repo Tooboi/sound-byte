e1feb6d8ad8602ebf806d435752ef21c08f23abc
Tooboi <japica123@gmail.com>
2023-02-10 22:36:03 -0500

    formating profile page

--- client/src/components/Tracks/LargeTrack.jsx
+++ client/src/components/Tracks/LargeTrack.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import {
+    Grid,
+    GridItem,
 
+} from '@chakra-ui/react'
 function LargeTrack({ post, user }) {
-
-    return (
-        <div>{post.artist} - {post.songName} - {post.createdAt} </div>
-    )
+  return (
+    <Grid h="100%" templateRows="248px 56px 1fr" templateColumns="2fr 0.8fr" gap={1}>
+      
+      <GridItem colSpan={2} rowSpan={1} bg="orange">hero</GridItem>
+      <GridItem colSpan={2} rowSpan={1} bg="maroon">tabs</GridItem>
+      <GridItem colSpan={1} rowSpan={1} rowStart={3} rowEnd={4} colStart={2} colEnd={3} minW="30%" bg="royalblue">stats</GridItem>
+      <GridItem rowSpan={1} colSpan={1} rowStart={3} rowEnd={4} bg="tomato">body</GridItem>
+    </Grid>
+  );
 }
 
 export default LargeTrack;
