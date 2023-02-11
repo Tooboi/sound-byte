fe524c2d67ada5d5ed8d56cb06513638305cc27b
Tooboi <japica123@gmail.com>
2023-02-11 00:06:34 -0500

    responsive profile page

--- client/src/components/Tracks/LargeTrack.jsx
+++ client/src/components/Tracks/LargeTrack.jsx
@@ -2,16 +2,18 @@ import React from 'react';
 import {
     Grid,
     GridItem,
-
+    Hide,
 } from '@chakra-ui/react'
 function LargeTrack({ post, user }) {
   return (
-    <Grid h="100%" templateRows="248px 56px 1fr" templateColumns="2fr 0.8fr" gap={1}>
+    <Grid h="100%" templateRows="156px 50px 1fr" templateColumns="2fr 0.8fr" gap={1}>
       
       <GridItem colSpan={2} rowSpan={1} bg="orange">hero</GridItem>
       <GridItem colSpan={2} rowSpan={1} bg="maroon">tabs</GridItem>
+      <Hide below="md">
       <GridItem colSpan={1} rowSpan={1} rowStart={3} rowEnd={4} colStart={2} colEnd={3} minW="30%" bg="royalblue">stats</GridItem>
-      <GridItem rowSpan={1} colSpan={1} rowStart={3} rowEnd={4} bg="tomato">body</GridItem>
+      </Hide>
+      <GridItem rowSpan={1} colSpan={[2, 2, 1, 1]} rowStart={3} rowEnd={4} bg="tomato">body</GridItem>
     </Grid>
   );
 }
