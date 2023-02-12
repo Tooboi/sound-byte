26139eadee330879f0b752d7fb2113e2fe11e93a
Tooboi <japica123@gmail.com>
2023-02-11 21:23:59 -0500

    temp img hero

--- client/src/components/Tracks/LargeTrack.jsx
+++ client/src/components/Tracks/LargeTrack.jsx
@@ -3,12 +3,15 @@ import {
     Grid,
     GridItem,
     Hide,
+    Image,
 } from '@chakra-ui/react'
 function LargeTrack({ post, user }) {
   return (
     <Grid h="100%" templateRows="156px 50px 1fr" templateColumns="2fr 0.8fr" gap={1}>
       
-      <GridItem colSpan={2} rowSpan={1} bg="orange">hero</GridItem>
+      <GridItem colSpan={2} rowSpan={1} overflow='hidden'>
+        <Image src="https://source.boringavatars.com/marble/1200?square" objectFit='cover'></Image>
+      </GridItem>
       <GridItem colSpan={2} rowSpan={1} bg="maroon">tabs</GridItem>
       <Hide below="md">
       <GridItem colSpan={1} rowSpan={1} rowStart={3} rowEnd={4} colStart={2} colEnd={3} minW="30%" bg="royalblue">stats</GridItem>
