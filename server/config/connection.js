39e7eadc157f1f4d1fc95e3f608a9d44b9e93f51
Tooboi <japica123@gmail.com>
2023-07-31 22:24:51 -0400

    connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/sound-byte',
+  process.env.MONGODB_URI 
+  // || 'mongodb://127.0.0.1:27017/sound-byte'
+  ,
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
